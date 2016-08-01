3cc827c4cd13d5a863bcd22c08ddc4a7aaa910cd
toxicwolf <toxic.wolf666@gmail.com>
2016-08-01 22:27:53 +0100

    Relicense under dual MIT/Apache-2.0 to match Rust ecosystem, add license headers and Cargo.toml metadata

--- sonar/src/lib.rs
+++ sonar/src/lib.rs
@@ -1,7 +1,15 @@
+// Copyright 2016 James Chapman
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #[cfg(feature = "window")]
 extern crate sonar_window;
 
 #[cfg(feature = "window")]
-mod window {
+pub mod window {
     pub use sonar_window::*;
 }
